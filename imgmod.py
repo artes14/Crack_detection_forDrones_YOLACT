7c0dbb5d237986a96b1221ad0e6bd5d2b5d3256c
artes14 <artes3412@gmail.com>
2021-05-10 11:00:59 +0900

    added imgmod.py

--- imgmod.py
+++ imgmod.py
@@ -2,6 +2,7 @@ import numpy as np
 import PIL
 from PIL import Image
 import glob
+import matplotlib.pyplot as plt
 import cv2
 
 def crop_image(img_path:str, crop_size:int):
@@ -16,3 +17,22 @@ def crop_image(img_path:str, crop_size:int):
     return crop_image
 
 
+def rm_g(img):
+    hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # green thres
+    lower_g=np.array([-10, 1,1])
+    higher_g = np.array([80,255,100])
+
+    # mask of image where there's green
+    mask = cv2.inRange(hsv, lower_g, higher_g)
+    fig, (ax1, ax2)=plt.subplots(2)
+
+    # green to gray..?
+    resultimg = cv2.bitwise_and(~img,~img, mask=mask)
+    result=img+resultimg
+    ax1.imshow(img)
+    ax2.imshow(result)
+    plt.show()
+    cv2.imwrite('crack_9_rmg.jpg', result)
+rm_g(cv2.imread('crack_9.jpg'))
